b916b93514babf75d96737f933f5ab7c57f2c629
v.stepanenko <mail@vitaliy.in>
2016-02-26 18:49:09 +0200

    Bootstrap: changing current directory to project_dir, this will allow to specify relative path for sqlite DB

--- bootstrap/bootstrap.php
+++ bootstrap/bootstrap.php
@@ -7,5 +7,6 @@ use Dotenv;
 require_once __DIR__ . '/functions.php';
 defined('PROJECT_DIR') || define('PROJECT_DIR', getProjectPath());
 defined('TESTING_HELPERS_DIR') || define('TESTING_HELPERS_DIR', dirname(__DIR__));
+chdir(PROJECT_DIR);
 require_once PROJECT_DIR . '/vendor/autoload.php';
 Dotenv::load(PROJECT_DIR, '.env');
