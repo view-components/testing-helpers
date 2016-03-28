d8b38dd95e05fe7aa84ddafaaa4ffb84a2fd8b21
v.stepanenko <mail@vitaliy.in>
2016-03-28 19:16:49 +0300

    DefaultFixture testing helper added

---
+++ src/Test/DefaultFixture.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace ViewComponents\TestingHelpers\Test;
+use Nayjest\Collection\Extended\ObjectCollection;
+
+class DefaultFixture
+{
+    private static $data;
+
+    public static function getArray()
+    {
+        if (self::$data === null) {
+            self::$data = include TESTING_HELPERS_DIR . '/resources/fixtures/users.php';
+        }
+        return self::$data;
+    }
+
+    public static function getObjects()
+    {
+        $objects = [];
+        foreach(self::getArray() as $arrayItem) {
+            $objects[] = (object)$arrayItem;
+        }
+        return $objects;
+    }
+
+    public static function getCollection()
+    {
+        return new ObjectCollection(self::getObjects());
+    }
+}
