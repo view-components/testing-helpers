49fb6aab2897234721ba84ec1cf86c29be0834ca
v.stepanenko <mail@vitaliy.in>
2016-04-05 19:39:46 +0300

    Fixtures update

--- resources/fixtures/db.sql
+++ resources/fixtures/db.sql
@@ -4,25 +4,25 @@ USE DB_NAME;
 
 DROP TABLE IF EXISTS test_users;
 CREATE TABLE IF NOT EXISTS test_users (
-  id int(10) NOT NULL,
+  id int NOT NULL,
   name varchar(255) NOT NULL,
   role varchar(31) NOT NULL,
   birthday date NOT NULL,
-  income int(10) NOT NULL,
+  income FLOAT NOT NULL,
   PRIMARY KEY (id)
 );
 
 DELETE FROM test_users;
 
-INSERT INTO test_users VALUES (1, 'John', 'Admin', '1970-01-16', 1201);
-INSERT INTO test_users VALUES (2, 'Max', 'Manager', '1980-11-20', 3050);
-INSERT INTO test_users VALUES (3, 'Anna', 'Manager', '1987-03-30', 20000);
+INSERT INTO test_users VALUES (1, 'John', 'Admin', '1970-01-16', 1201.1);
+INSERT INTO test_users VALUES (2, 'Max', 'Manager', '1980-11-20', 3050.5);
+INSERT INTO test_users VALUES (3, 'Anna', 'Manager', '1987-03-30', 20000.33);
 INSERT INTO test_users VALUES (4, 'Lisa', 'User', '1989-04-21', 120);
 INSERT INTO test_users VALUES (5, 'Eric', 'User', '1990-10-23', 59310);
 INSERT INTO test_users VALUES (6, 'David', 'User', '1990-10-23', 29526);
 INSERT INTO test_users VALUES (7, 'Bruce', 'User', '1977-09-14', 12783);
 INSERT INTO test_users VALUES (8, 'Julia', 'User', '1994-03-05', 34);
-INSERT INTO test_users VALUES (9, 'Ben', 'User', '1991-11-13', 0);
+INSERT INTO test_users VALUES (9, 'Ben', 'User', '1991-11-13', 0.15);
 INSERT INTO test_users VALUES (10, 'Frank', 'Manager', '1964-10-29', 3453);
 INSERT INTO test_users VALUES (11, 'Phil', 'User', '1972-08-17', 4172);
 INSERT INTO test_users VALUES (12, 'Nikita', 'User', '1973-04-17', 10);
@@ -37,4 +37,4 @@ INSERT INTO test_users VALUES (19, 'Diego', 'User', '1987-05-11', 70001);
 INSERT INTO test_users VALUES (20, 'Robert', 'Admin', '1984-02-01', 0);
 INSERT INTO test_users VALUES (21, 'Peter', 'User', '1994-05-12', 3461678);
 INSERT INTO test_users VALUES (22, 'Sebastian', 'User', '1991-11-16', 10);
-INSERT INTO test_users VALUES (23, 'Rafael', 'User', '1993-05-04', 2514);
+INSERT INTO test_users VALUES (23, 'Rafael', 'User', '1993-05-04', 2514.18);
--- resources/fixtures/users.php
+++ resources/fixtures/users.php
@@ -1,14 +1,14 @@
 <?php
 return [
-    ['id' => '1', 'name' => 'John', 'role' => 'Admin', 'birthday' => '1970-01-16', 'income' => 1201],
-    ['id' => '2', 'name' => 'Max', 'role' => 'Manager', 'birthday' => '1980-11-20', 'income' => 3050],
-    ['id' => '3', 'name' => 'Anna', 'role' => 'Manager', 'birthday' => '1987-03-30', 'income' => 20000],
+    ['id' => '1', 'name' => 'John', 'role' => 'Admin', 'birthday' => '1970-01-16', 'income' => 1201.1],
+    ['id' => '2', 'name' => 'Max', 'role' => 'Manager', 'birthday' => '1980-11-20', 'income' => 3050.5],
+    ['id' => '3', 'name' => 'Anna', 'role' => 'Manager', 'birthday' => '1987-03-30', 'income' => 20000.33],
     ['id' => '4', 'name' => 'Lisa', 'role' => 'User', 'birthday' => '1989-04-21', 'income' => 120],
     ['id' => '5', 'name' => 'Eric', 'role' => 'User', 'birthday' => '1990-10-23', 'income' => 59310],
     ['id' => '6', 'name' => 'David', 'role' => 'User', 'birthday' => '1967-04-09', 'income' => 29526],
     ['id' => '7', 'name' => 'Bruce', 'role' => 'User', 'birthday' => '1977-09-14', 'income'=> 12783],
     ['id' => '8', 'name' => 'Julia', 'role' => 'User', 'birthday' => '1994-03-05', 'income' => 34],
-    ['id' => '9', 'name' => 'Ben', 'role' => 'User', 'birthday' => '1991-11-13', 'income' => 0],
+    ['id' => '9', 'name' => 'Ben', 'role' => 'User', 'birthday' => '1991-11-13', 'income' => 0.15],
     ['id' => '10', 'name' => 'Frank', 'role' => 'Manager', 'birthday' => '1964-10-29', 'income' => 3453],
     ['id' => '11', 'name' => 'Phil', 'role' => 'User', 'birthday' => '1972-08-17', 'income' => 4172],
     ['id' => '12', 'name' => 'Nikita', 'role' => 'User', 'birthday' => '1973-04-17', 'income' => 10],
@@ -22,5 +22,5 @@ return [
     ['id' => '20', 'name' => 'Robert', 'role' => 'Admin', 'birthday' => '1984-02-01', 'income' => 0],
     ['id' => '21', 'name' => 'Peter', 'role' => 'User', 'birthday' => '1994-05-12', 'income' => 3461678],
     ['id' => '22', 'name' => 'Sebastian', 'role' => 'User', 'birthday' => '1991-11-16', 'income' => 10],
-    ['id' => '23', 'name' => 'Rafael', 'role' => 'User', 'birthday' => '1993-05-04', 'income' => 2514],
+    ['id' => '23', 'name' => 'Rafael', 'role' => 'User', 'birthday' => '1993-05-04', 'income' => 2514.18],
 ];
