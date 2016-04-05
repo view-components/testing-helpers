dbb9cee8f67cf6754701b464af2a83cdbb7a2e59
v.stepanenko <mail@vitaliy.in>
2016-04-05 17:44:34 +0300

    Layout fixes

--- resources/views/layout.php
+++ resources/views/layout.php
@@ -10,7 +10,7 @@ use ViewComponents\ViewComponents\Component\Layout;
 </head>
 <body>
 <?= $layout->section('menu')->render() ?>
-<div class="container" style="max-width: 1170px;">
+<div class="container">
     <div style="margin: 10px">
         <h1>Demo App<?= $title ? "<small> \\\\ $title</small>" : '' ?></h1>
         <hr/>
@@ -19,5 +19,13 @@ use ViewComponents\ViewComponents\Component\Layout;
 </div>
 <?= $layout->section('footer')->render() ?>
 <?php include __DIR__ . '/timing.php' ?>
+<style>
+    .container {
+        margin-right: auto;
+        margin-left: auto;
+        max-width: 1170px;
+        padding: 10px;
+    }
+</style>
 </body>
 </html>
